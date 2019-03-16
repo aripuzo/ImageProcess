69616347bbee1eea35df319c76f03d85eeccb56d
Ari Tamunomieni <aribiton@betazoo.com.ng>
2019-03-16 09:31:19 +0100

    Initial Commit

---
+++ ImageProcess/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ImageProcess
+//
+//  Created by thetaeo on 16/03/2019.
+//  Copyright © 2019 Taleteam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
